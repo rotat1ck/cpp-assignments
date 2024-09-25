406373503cbe61864b9bcfaa2bdc657ff67a1390
rotatick <rotatick32151@gmail.com>
2024-09-26 02:53:55 +0300

    chess king

--- main.cpp
+++ main.cpp
@@ -2,7 +2,46 @@
 
 using namespace std;
 
-int main() 
-{
-    
-} 
+int main() {
+    int starta, startb, a, b, num = 0;
+    cin >> starta >> startb >> a >> b;
+
+    if (starta <= a) {
+        if ((a - starta) <= 1) {
+            if (startb <= b) {
+                if ((b - startb) <= 1) {
+                    cout << "YES";
+                } else {
+                    cout << "NO";
+                }
+            } else {
+                if ((startb - b) <= 1) {
+                    cout << "YES";
+                } else {
+                    cout << "NO";
+                }
+            }
+        } else {
+            cout << "NO";
+        }
+    } else if (starta >= a) {
+        if ((starta - a) <= 1) {
+            if (startb <= b) {
+                if ((b - startb) <= 1) {
+                    cout << "YES";
+                } else {
+                    cout << "NO";
+                }
+            } else {
+                if ((startb - b) <= 1) {
+                    cout << "YES";
+                } else {
+                    cout << "NO";
+                }
+            }
+        } else {
+            cout << "NO";
+        }
+    }
+    return 0;
+}
