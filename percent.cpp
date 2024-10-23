1273c2eafb7fbeb9dbc15d4f6f1be69ed1785d86
rotatick <rotatick32151@gmail.com>
2024-10-23 21:12:58 +0300

    banking percent

--- percent.cpp
+++ percent.cpp
@@ -7,10 +7,8 @@ void getvalue(double *x, double* y, double* p ) {
     double a = (*x/100)**p;
     double b = ((*y/100)**p) + ((a-(int)a)* 100);
 
-<<<<<<< HEAD
     cout << a << " " << b << " проценты" << endl;
-=======
->>>>>>> 3a0cd425b5ed7c7dc4de5e6d948fc85ac0549759
+
     if (b >= 100) {
         a += 1;
         b -= 100;
@@ -29,11 +27,9 @@ int main() {
 
     for (int i = 1; i <= z; ++i) {
         getvalue(&x, &y, &p);
-<<<<<<< HEAD
         cout << x << " " << y << endl;
-=======
->>>>>>> 3a0cd425b5ed7c7dc4de5e6d948fc85ac0549759
     }
+    
     if (p == 0) {
         cout << (int)x << " " << (int)y;
         return 0;
