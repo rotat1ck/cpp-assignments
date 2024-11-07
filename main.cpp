52efd6a812b625746eda0813339bb95d9fe12c8f
rotat1ck <rotatick32151@gmail.com>
2024-11-07 13:02:10 +0300

    need modification

--- main.cpp
+++ main.cpp
@@ -1,33 +1,45 @@
 #include <iostream>
 #include <vector>
+#include <conio.h>
+#include <windows.h>
 
 using namespace std;
 
+system("chcp 65001");
+system("cls");
+
+void one();
+
 
 int main() {
-    int n;
-    cin >> n;
-    vector <int> a;
-    //считывание
-    for (int i = 0; i < n; i++) {
-        int temp;
-        cin >> temp;
-        a.push_back(temp);
-    }
-    //обработка
-    int num_min = 0; //номер минимального элемента
-    for (int i = 0; i < n; i++) {
-        if (a[i] < a[num_min]) {
-            num_min = i;
+    cout << "Выберите номер задания от 1 до 7: ";
+    while (true) {
+        if (kbhit()) {
+            switch(getch()) {
+                case 49:
+                    cout << 1 << endl;
+                    one();
+                    break;
+            }
         }
     }
-    //обмен значений элементов a[0] и a[num_min]
-    int temp;
-    temp = a[0];
-    a[0] = a[num_min];
-    a[num_min] = temp;
-    //вывод
-    for (auto now : a) {
-        cout << now << " ";
+
+}
+
+void one() {
+    int a,b;
+    system("cls");
+    cout << "Введите два числа: ";
+    cin >> a >> b;
+    
+    while (a != b) {
+        if (a > b) {
+            a = a - b;
+        } else {
+            b = b - a;
+        }
     }
+
+    cout << "Наибольший общий делитель: " << a << endl;
+    main();
 }
