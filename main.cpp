16714d6487b2b0b70a13c783aea5a9da237bb1c7
rotat1ck <rotatick32151@gmail.com>
2024-11-07 18:42:47 +0300

    3 of 7 completed

--- main.cpp
+++ main.cpp
@@ -2,27 +2,56 @@
 #include <vector>
 #include <conio.h>
 #include <windows.h>
+#include <unordered_map>
 
 using namespace std;
 
-system("chcp 65001");
-system("cls");
+unordered_map<char, string> morseCodeMap = {
+    {'A', ".-"}, {'B', "-..."}, {'C', "-.-."}, {'D', "-.."},
+    {'E', "."}, {'F', "..-."}, {'G', "--."}, {'H', "...."},
+    {'I', ".."}, {'J', ".---"}, {'K', "-.-"}, {'L', ".-.."},
+    {'M', "--"}, {'N', "-."}, {'O', "---"}, {'P', ".--."},
+    {'Q', "--.-"}, {'R', ".-."}, {'S', "..."}, {'T', "-"},
+    {'U', "..-"}, {'V', "...-"}, {'W', ".--"}, {'X', "-..-"},
+    {'Y', "-.--"}, {'Z', "--.."},
+    {'0', "-----"}, {'1', ".----"}, {'2', "..---"}, {'3', "...--"},
+    {'4', "....-"}, {'5', "....."}, {'6', "-...."}, {'7', "--..."},
+    {'8', "---.."}, {'9', "----."},
+    {' ', "/"}
+};
 
 void one();
+void two();
+void three();
+void four();
+void five();
+void six();
+void seven();
 
-
-int main() {
+int start() {
     cout << "Выберите номер задания от 1 до 7: ";
     while (true) {
         if (kbhit()) {
             switch(getch()) {
                 case 49:
-                    cout << 1 << endl;
                     one();
+                    break;
+                case 50:
+                    two();
+                    break;
+                case 51:
+                    three();
                     break;
             }
         }
     }
+}
+
+
+int main() {
+    system("chcp 65001");
+    system("cls");
+    start();
 
 }
 
@@ -41,5 +70,82 @@ void one() {
     }
 
     cout << "Наибольший общий делитель: " << a << endl;
-    main();
+    start();
+}
+
+void two() {
+    system("cls");
+    vector<int> nums; 
+    cout << "Введите числа через ENTER в массив (ESC - закончить ввод): " << endl;
+    char ch;
+    bool noesc = true;
+    int converted = 0;
+
+    while (noesc) {
+        if (_kbhit()) {
+            ch = _getch();
+
+            if (ch == 27) {
+                noesc = false;
+                break;
+            } else if (ch == '\r') {
+                if (converted != 0) {
+                    nums.push_back(converted);
+                    cout << endl;
+                    converted = 0;
+                }
+            } else if (ch >= '0' && ch <= '9') {
+                converted = converted * 10 + (ch - '0');
+                cout << ch;
+            }
+        }
+    }
+
+    cout << "\nВведенный массив: ";
+    for (auto num : nums) {
+        cout << num << " ";
+    }
+
+    cout << "\nПеревернутый массив: ";
+    for(int i = nums.size()-1; i >= 0; --i) {
+        cout << nums[i] << " ";
+    }
+    
+    cout << endl;
+    start();
+}
+
+void three() {
+    system("cls");
+    cout << "Введите текст для преобразования в морзянку (ENTER - закончить ввод): " << endl;
+    char ch;
+    bool noesc = true;
+    string inputText;
+    
+    while (noesc) {
+        if (_kbhit()) {
+            ch = _getch();
+
+            if (ch == '\r') {
+                cout << endl;
+                break;
+            } else {
+                inputText += toupper(ch);
+                cout << ch;
+            }
+        }
+    }
+
+
+    cout << "\nМорзянка: ";
+    for (char c : inputText) {
+        if (morseCodeMap.find(c) != morseCodeMap.end()) {
+            cout << morseCodeMap[c] << " "; 
+        } else {
+            cout << "? "; 
+        }
+    }
+    
+    cout << endl;
+    start();
 }
