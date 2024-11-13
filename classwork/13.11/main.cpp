73f57ca064468718e644c6becc67bd75eb5ba762
rotat1ck <rotatick32151@gmail.com>
2024-11-13 15:30:29 +0300

    test

---
+++ classwork/13.11/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+double acc(vector<double> qs) {
+    double a = 0;
+    for (double q : qs) {
+        a += q;
+    }
+    return 0;
+}
+
+int main() {
+
+}
