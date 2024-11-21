f810a3d262abe669a8b22706b38615a158c0b41c
rotat1ck <rotatick32151@gmail.com>
2024-11-21 15:12:01 +0300

    class

--- classwork/13.11/main.cpp
+++ classwork/13.11/main.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
 #include <vector>
+#include <memory>
 
 using namespace std;
 
-double acc(vector<double> qs) {
-    double a = 0;
-    for (double q : qs) {
-        a += q;
+double avg(const vector<double>& qs) {
+    double acc = 0;
+    for (double i : qs) {
+        acc += i;
     }
-    return 0;
+    return acc / qs.size();
+}
+
+void avg(const vector<double>& qs, double &a) {
+    double acc = 0;
+    for (double i : qs) {
+        acc += i;
+    }
+    a = acc / qs.size();
+    cout << a << endl;
 }
 
 int main() {
-
+    vector<double> rds{345, 34.23, 1.0/2.0, 441};  
+    vector<int> ris{345, 34, 1, 441}; 
+    double a;
+    avg(rds, a);
+    cout << avg(rds) << endl;
 }
---
+++ classwork/lambda/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int countChet(const vector<int> &qs) {
+    return count_if(qs.begin(), qs.end(), [](int a) -> bool { return a % 2 == 0; });
+}
+
+int countN(const vector<int> &qs, int &n) {
+    return count_if(qs.begin(), qs.end(), [=](int a) -> bool { return a % n == 0; });
+}
+
+void map(vector<int> &qs, int func(int, int)) {
+    for (int i = 0; i < qs.size(); ++i) {
+        qs[i] = func(qs[i], i);
+    }
+}
+
+int main() {
+    vector<int> nums{10, 23, 34, 45, 56, 147};
+    for (int i : nums) { cout << i << " ";} cout << endl;
+    map(nums, [](int item, int index) { return item + index;});
+    for (int i : nums) { cout << i << " ";}
+}
