0f7efa7e8631413ff5ecf1881c6671d3592d4d73
rotat1ck <rotatick32151@gmail.com>
2025-01-17 15:53:28 +0300

    math functions fixes

--- classwork/2025.01/math/main.cpp
+++ classwork/2025.01/math/main.cpp
@@ -6,32 +6,34 @@
 
 using namespace std;
 
-double one(double x, double y) {
-    const double pi = M_PI;
-    x = (x / 180) * pi;
-    y = (y / 180) * pi;
+const double Pi = asin(1.0), g = 9.81;
+
+double one(double &x, double &y) {
+    x = (x / 180) * Pi;
+    y = (y / 180) * Pi;
     return pow(1 - tan(x), (cos(x) / sin(x))) + cos(x-y);
 }
 
-double two(double v, double t) {
-    return asin(10 * t / 2 * v);
+double two(double &v, double &t) {
+    const double a = asin(g * t / (2 * v));
+    return a * (90 / Pi);
 }
 
 int three(int num) {
     vector<int> nums;
     while (num > 0) {
         nums.push_back(num % 10);
-        num = num % 10;
+        num = num / 10;
     }
     return *max_element(nums.begin(), nums.end());
 }
 
-double four(vector<float> &arr, int size) {
+double four(vector<float> &arr, int &size) {
     double sum = accumulate(arr.begin(), arr.end(), 0);
     return sum / size;
 }
 
-int five(int limit) {
+int five(int &limit) {
     int result = 0;
     int ptr = 0;
     while (ptr <= limit) {
