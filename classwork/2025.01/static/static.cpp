5c2d3f2f8c24f8494963d14e6f9e3ad5a59c6185
rotat1ck <rotatick32151@gmail.com>
2025-01-21 12:27:51 +0300

    21.01 static

--- classwork/2025.01/static/static.cpp
+++ classwork/2025.01/static/static.cpp
@@ -3,28 +3,57 @@
 
 using namespace std;
 
-class users {
+class DB {
+protected:
+    static string db_name;
+    static string ip;
+    static int port;
+    static string username;
+    static string pass;
 public:
-    static int userCount;
+    static void connect(string _username, string _pass) {
+        if (_username == username && _pass == pass) {
+            cout << "Подключание успешно xD" << endl;
+            cout << "DB Name: " << db_name << endl;
+            cout << "IP: " << ip << endl;
+            cout << "Port: " << port << endl;
+        } else {
+            cout << "Не удалось подключаиться" << endl;
+        }
+    }
+};
 
+class users : DB {
+private:
+    static int userCount;
+public:
     static int getUserCount() {
         return userCount;
     }
     static void changeUserCount(int n) {
         userCount = n;
     }
+    static string getDbName() {
+        return db_name;
+    }
 };
 
-int users::userCount = 5;
+string DB::db_name = "prod";
+string DB::ip = "77.77.77.77";
+int DB::port = 7777;
+string DB::username = "admin";
+string DB::pass = "pass";
+int users::userCount = 0;
 
 int main() {
     Timer::startTimer();
 
-    cout << users::getUserCount() << endl;
+    DB::connect("admin", "pass");
 
+    cout << users::getUserCount() << endl;
     users::changeUserCount(15);
-
     cout << users::getUserCount() << endl;
+    cout << users::getDbName();
 
     Timer::stopTimer();
 }
